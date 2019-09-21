f20d29312bd6bb65482b2ef2283658c219c71ce5
Michael Kennedy <mikeckennedy@gmail.com>
2019-09-21 10:37:11 -0700

    refactored to better separation.

--- src/fitnessd_client/fitnessd.py
+++ src/fitnessd_client/fitnessd.py
@@ -1,40 +1,22 @@
 import datetime
-from typing import Optional
 
-import requests
-
-base_url = 'https://fresh-zealous-song.anvil.app/_/api/'
+import svc
 
 
 def main():
     auth_data = get_auth_data()
-    api_key = authenticate(auth_data)
+    api_key = svc.authenticate(auth_data)
     if not api_key:
         print("Invalid login")
         return
 
     data = get_user_data()
     print(api_key)
-    result = save_measurement(api_key, auth_data.get('email'), data)
+    result = svc.save_measurement(api_key, auth_data.get('email'), data)
     if result:
         print("Done!")
     else:
         print("Could not save measurement")
-
-
-def authenticate(data: dict) -> Optional[str]:
-    email = data.get('email')
-    password = data.get('password')
-    body = {
-        'email': email,
-        'password': password,
-    }
-    url = base_url + 'authorize'
-    resp = requests.post(url, json=body)
-    if not resp.status_code == 200:
-        return None
-
-    return resp.json().get('api_key')
 
 
 def get_user_data() -> dict:
@@ -58,19 +40,6 @@ def get_auth_data() -> dict:
         'email': email,
         'password': password,
     }
-
-
-def save_measurement(api_key: str, email: str, data: dict):
-    url = base_url + 'add_measurement'
-    auth = {
-        "email": email,
-        "api_key": api_key,
-    }
-    data.update(auth)
-
-    resp = requests.post(url, json=data)
-    print("Server response", resp.text)
-    return resp.status_code == 200
 
 
 if __name__ == '__main__':
---
+++ src/fitnessd_client/svc.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+import requests
+
+base_url = 'https://fresh-zealous-song.anvil.app/_/api/'
+
+
+def authenticate(data: dict) -> Optional[str]:
+    email = data.get('email')
+    password = data.get('password')
+    body = {
+        'email': email,
+        'password': password,
+    }
+    url = base_url + 'authorize'
+    resp = requests.post(url, json=body)
+    if not resp.status_code == 200:
+        return None
+
+    return resp.json().get('api_key')
+
+
+def save_measurement(api_key: str, email: str, data: dict):
+    url = base_url + 'add_measurement'
+    auth = {
+        "email": email,
+        "api_key": api_key,
+    }
+    data.update(auth)
+
+    resp = requests.post(url, json=data)
+    print("Server response", resp.text)
+    return resp.status_code == 200
